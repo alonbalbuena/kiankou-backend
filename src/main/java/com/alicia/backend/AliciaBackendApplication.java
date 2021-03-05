3625416c1aa1d29ba64c439b172eeaa0cf14309f
alonbalbuena <alonbalbuena@gmail.com>
2021-03-05 23:58:21 +0100

    :tada: initialize repository

---
+++ src/main/java/com/alicia/backend/AliciaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alicia.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliciaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliciaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alicia/backend/AliciaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alicia.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliciaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

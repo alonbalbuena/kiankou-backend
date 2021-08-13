ad1fa5e6b38585b083016f3f639ce86a6c92dd51
alonbalbuena <alonbalbuena@gmail.com>
2021-08-14 00:54:14 +0200

    remove dependencies

--- src/main/java/com/alicia/backend/movies/controller/RestController.java
+++ src/main/java/com/alicia/backend/movies/controller/RestController.java
@@ -1,8 +1,10 @@
 package com.alicia.backend.movies.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.web.bind.annotation.RestController
+@RequestMapping("/movies")
 public class RestController {
 
 	@GetMapping
